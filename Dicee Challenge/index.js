98eee33dacb7f8d8893d67110db01fa09f889222
aleyandros <53386652+aleyandros@users.noreply.github.com>
2022-12-30 02:09:37 +0000

    Dicee game finished

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,11 +1,16 @@
-alert("Done!!");
+//alert("Done!!");
 var randomNumber1 = Math.floor( Math.random() * 6) +1;
 var randomNumber2 = Math.floor( Math.random() * 6) +1;
 console.log (randomNumber1);
 
-var diceImg = document.querySelectorAll("img");
-diceImg[0].setAttribute("src", "images/dice"+ randomNumber1+ ".png");
-diceImg[1].setAttribute("src", "images/dice"+ randomNumber2+ ".png");
+var dice0 = document.querySelectorAll("img")[0];
+var dice1 = document.querySelectorAll("img")[1];
+
+var diceImg0 = "images/dice"+ randomNumber1+ ".png";
+var diceImg1 = "images/dice"+ randomNumber2+ ".png";
+
+dice0.setAttribute("src", diceImg0);
+dice1.setAttribute("src", diceImg1);
 
 var winner = randomNumber1 > randomNumber2 ? "Player 1 Wins!" : (randomNumber1 < randomNumber2 ? "Player 2 Wins!" : "Draw");
 
