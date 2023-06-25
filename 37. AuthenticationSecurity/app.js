795278216cde722708f693e9b4a9ca3376c84a60
Alex Patiño <ayopatincha@gmail.com>
2023-06-24 21:39:05 -0500

    Section 37: Authentication & Security - Add Environment Vars

--- 37. AuthenticationSecurity/app.js
+++ 37. AuthenticationSecurity/app.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -14,6 +17,21 @@ app.use(
   })
 );
 
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function (req, res) {
   res.render("home");
 });
@@ -24,6 +42,39 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(function () {
+      res.render("secrets");
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const pass = req.body.password;
+
+  User.findOne({ email: username }).then((foundUser) => {
+    if (foundUser) {
+      if (foundUser.password == pass) {
+        res.render("secrets");
+      } else {
+        res.send("wrong password");
+      }
+    } else {
+      res.send("user not found");
+    }
+  });
 });
 
 const port = 3000;
